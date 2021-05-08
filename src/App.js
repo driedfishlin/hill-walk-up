896acfb4bcdc93c28d0d03a58bc49e95c718e7a0
driedfish <driedfishlin@gmail.com>
2021-05-08 22:58:42 +0800

    introduce flow

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-function App() {
+// @flow
+import * as React from 'react';
+
+const App = function(): React.Node {
 	return (
-		<div class="text-center text-4xl text-white">React with Tailwind</div>
+		<div className="text-center text-4xl text-white">
+			React with Tailwind
+		</div>
 	);
-}
+};
 
 export default App;
