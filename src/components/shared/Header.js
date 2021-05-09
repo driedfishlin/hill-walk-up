cfce7b4882c83c9946b203cf24332e1d043ed790
driedfish <driedfishlin@gmail.com>
2021-05-10 02:36:20 +0800

    header style modification

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 
 const Button = function(): React.Node {
 	return (
-		<button className="flex flex-col justify-between py-2 px-1.5 text-white w-8 h- focus:outline-none">
+		<button className="flex flex-col justify-between py-2 px-1.5 text-white w-8 h-8 focus:outline-none">
 			<div className="w-full h-0.5 bg-white rounded-sm" />
 			<div className="w-full h-0.5 bg-white rounded-sm" />
 			<div className="w-full h-0.5 bg-white rounded-sm" />
@@ -13,10 +13,12 @@ const Button = function(): React.Node {
 
 const Header = function(): React.Node {
 	return (
-		<header className="flex justify-between px-3 py-2 bg-t-green shadow-sm">
+		<header className="flex justify-between items-center px-3 py-2 bg-t-green">
 			<h1 className="text-xl text-white tracking-wider noto-sans font-medium">
 				走走山岳
-				<span className="text-xs tracking-wide ml-1">HELL WALK UP</span>
+				<span className="block text-xs tracking-wide">
+					HELL WALK UP
+				</span>
 			</h1>
 			<Button />
 		</header>
